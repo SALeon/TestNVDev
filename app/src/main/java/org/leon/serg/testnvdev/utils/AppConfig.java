f037b2f90870da5ebdd6dc673ae7284468afa30e
SALeon <developleon@gmail.com>
2017-05-12 19:19:42 +0300

    Update AppConfig.java

--- app/src/main/java/org/leon/serg/testnvdev/utils/AppConfig.java
+++ app/src/main/java/org/leon/serg/testnvdev/utils/AppConfig.java
@@ -11,5 +11,5 @@ public interface  AppConfig {
 
   public static final String  MAX_WITH_PHOTO_URL_ATR="?maxwidth=";
   public static final String  REFERENCE_PHOTO_URL_ATR="&photoreference=";
-  public static final String  KEY_URL_ATR="&key";
+  public static final String  KEY_URL_ATR="&key=";
 }
